53fcd5bd3ccf5782a132d77628d0d642b5ce8064
Apple <your-real-email@example.com>
2026-07-28 13:31:31 +0300

    Use custom login view

--- accounts/views.py
+++ accounts/views.py
@@ -1,1 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import messages
+
+
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("select_company")
+
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("select_company")
+        else:
+            messages.error(request, "Invalid username or password.")
+    else:
+        form = AuthenticationForm()
+
+    return render(request, "registration/login.html", {"form": form})
